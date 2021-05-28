173ecfdc57f22bef2be4e4d5dfb86df2d8c90981
diemkhang1912@gmail.com <diemkhang1912@gmail.com>
2021-05-28 08:08:00 +0700

    activity

--- src/vn/plusplus/activity33.java
+++ src/vn/plusplus/activity33.java
@@ -7,16 +7,17 @@ public class activity33 {
         //Nhập 1 số epsilon tìm giai thừa đến số đó và in ra kq của e = 1+1+1/1!+1/2!+...+1/epsilon!
         Scanner input = new Scanner(System.in);
         System.out.print("Nhap mot so Epsilon < 1: ");
-        int epsilon = input.nextInt();
+        double epsilon = input.nextDouble();
         double e =0;
 
-        for(int i =0;i<=epsilon;i++){
-            int gt =1;
+        for(int i =0;;i++){
+            int n =1;
             for(int j=1;j<=i;j++){
-                gt*=j;
+                n*=j;
             }
-            System.out.println(i+"!"+" = "+gt);
-            e = e + (double) 1/gt;
+            System.out.println(i+"!"+" = "+n);
+            e = e + (double) 1/n;
+            if((double)1/n < epsilon ) break;
         }
         System.out.println("Epsilon = "+ epsilon+" va e = "+Math.round(e*1000.0)/1000.0);
     }
